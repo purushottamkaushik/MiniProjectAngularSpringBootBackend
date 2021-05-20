09b4194d416b0539f182bce59ce64e9b97f35a7e
purushottamkaushik <purushottamkaushik96@gmail.com>
2021-05-20 20:45:13 +0530

    Initial Demo Commit

---
+++ src/main/java/com/example/springbootminiprojectangular/SpringBootMiniProjectAngularApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootminiprojectangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMiniProjectAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMiniProjectAngularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootminiprojectangular/SpringBootMiniProjectAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootminiprojectangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMiniProjectAngularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
